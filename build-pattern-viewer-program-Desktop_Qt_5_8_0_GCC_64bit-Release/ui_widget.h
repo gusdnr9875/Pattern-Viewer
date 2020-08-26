12eab9d2da1de6906db8840e7d4b797945589721
gusdnr9875 <gusdnr9875@naver.com>
2020-08-26 13:57:03 +0900

    exe

---
+++ build-pattern-viewer-program-Desktop_Qt_5_8_0_GCC_64bit-Release/ui_widget.h
@@ -0,0 +1,141 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.8.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QTextEdit *textedit;
+    QLabel *label;
+    QLabel *label_1;
+    QLineEdit *keyword1;
+    QPushButton *search_button1;
+    QPushButton *nextbutton1;
+    QTextEdit *textedit2;
+    QLabel *label_2;
+    QLineEdit *keyword2;
+    QPushButton *search_button2;
+    QPushButton *nextbutton2;
+    QLabel *label_3;
+    QPushButton *fileSelect;
+    QLabel *filename;
+    QPushButton *close;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QStringLiteral("Widget"));
+        Widget->resize(1410, 870);
+        textedit = new QTextEdit(Widget);
+        textedit->setObjectName(QStringLiteral("textedit"));
+        textedit->setGeometry(QRect(20, 70, 501, 671));
+        textedit->setUndoRedoEnabled(true);
+        label = new QLabel(Widget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(20, 20, 231, 41));
+        label->setBaseSize(QSize(3, 4));
+        QFont font;
+        font.setFamily(QStringLiteral("Carlito"));
+        font.setBold(true);
+        font.setItalic(true);
+        font.setWeight(75);
+        label->setFont(font);
+        label_1 = new QLabel(Widget);
+        label_1->setObjectName(QStringLiteral("label_1"));
+        label_1->setGeometry(QRect(30, 770, 61, 31));
+        label_1->setBaseSize(QSize(3, 4));
+        label_1->setFont(font);
+        keyword1 = new QLineEdit(Widget);
+        keyword1->setObjectName(QStringLiteral("keyword1"));
+        keyword1->setEnabled(true);
+        keyword1->setGeometry(QRect(100, 770, 321, 28));
+        search_button1 = new QPushButton(Widget);
+        search_button1->setObjectName(QStringLiteral("search_button1"));
+        search_button1->setGeometry(QRect(440, 770, 84, 28));
+        nextbutton1 = new QPushButton(Widget);
+        nextbutton1->setObjectName(QStringLiteral("nextbutton1"));
+        nextbutton1->setEnabled(false);
+        nextbutton1->setGeometry(QRect(440, 810, 84, 28));
+        textedit2 = new QTextEdit(Widget);
+        textedit2->setObjectName(QStringLiteral("textedit2"));
+        textedit2->setGeometry(QRect(630, 70, 501, 671));
+        textedit2->setUndoRedoEnabled(true);
+        label_2 = new QLabel(Widget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(650, 770, 61, 31));
+        label_2->setBaseSize(QSize(3, 4));
+        label_2->setFont(font);
+        keyword2 = new QLineEdit(Widget);
+        keyword2->setObjectName(QStringLiteral("keyword2"));
+        keyword2->setEnabled(true);
+        keyword2->setGeometry(QRect(720, 770, 321, 28));
+        search_button2 = new QPushButton(Widget);
+        search_button2->setObjectName(QStringLiteral("search_button2"));
+        search_button2->setGeometry(QRect(1050, 770, 84, 28));
+        nextbutton2 = new QPushButton(Widget);
+        nextbutton2->setObjectName(QStringLiteral("nextbutton2"));
+        nextbutton2->setEnabled(false);
+        nextbutton2->setGeometry(QRect(1050, 810, 84, 28));
+        label_3 = new QLabel(Widget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(1160, 840, 231, 20));
+        fileSelect = new QPushButton(Widget);
+        fileSelect->setObjectName(QStringLiteral("fileSelect"));
+        fileSelect->setGeometry(QRect(1230, 760, 151, 28));
+        filename = new QLabel(Widget);
+        filename->setObjectName(QStringLiteral("filename"));
+        filename->setGeometry(QRect(30, 840, 1031, 20));
+        close = new QPushButton(Widget);
+        close->setObjectName(QStringLiteral("close"));
+        close->setGeometry(QRect(1230, 800, 151, 28));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", Q_NULLPTR));
+        label->setText(QApplication::translate("Widget", "  Pattern Viewer Program", Q_NULLPTR));
+        label_1->setText(QApplication::translate("Widget", "Keyword:", Q_NULLPTR));
+        search_button1->setText(QApplication::translate("Widget", "Find", Q_NULLPTR));
+        nextbutton1->setText(QApplication::translate("Widget", "Next", Q_NULLPTR));
+        label_2->setText(QApplication::translate("Widget", "Keyword:", Q_NULLPTR));
+        search_button2->setText(QApplication::translate("Widget", "Find", Q_NULLPTR));
+        nextbutton2->setText(QApplication::translate("Widget", "Next", Q_NULLPTR));
+        label_3->setText(QApplication::translate("Widget", "Copyright \302\251UNITEST Corporation", Q_NULLPTR));
+        fileSelect->setText(QApplication::translate("Widget", "File Select", Q_NULLPTR));
+        filename->setText(QApplication::translate("Widget", "dfdsadasdasdasdasd", Q_NULLPTR));
+        close->setText(QApplication::translate("Widget", "Close", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
